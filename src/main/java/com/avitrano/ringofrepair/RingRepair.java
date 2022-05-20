c2ee9ff659e245e1dff07a0f4aa69fe3359a3e6b
HolyHades <holyhades64@gmail.com>
2022-05-20 08:26:53 +0200

    update sprite, fixed delay

--- src/main/java/com/avitrano/ringofrepair/RingRepair.java
+++ src/main/java/com/avitrano/ringofrepair/RingRepair.java
@@ -6,8 +6,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 
-import static com.avitrano.ringofrepair.Ringofrepair.REPAIR_DELAY;
-
 public class RingRepair extends Item {
     public RingRepair(Settings settings) {
         super(settings);
@@ -16,7 +14,7 @@ public class RingRepair extends Item {
     public static void repair(World world, PlayerEntity player) {
         if (!world.isClient) {
             ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-            if (serverPlayer.age % world.getGameRules().getInt(REPAIR_DELAY) == 0) {
+            if (serverPlayer.age % 40 == 0) {
                 for (int i = 0; i < serverPlayer.getInventory().size(); i++) {
                     ItemStack stack = serverPlayer.getInventory().getStack(i);
                     if (!stack.isEmpty()) {
